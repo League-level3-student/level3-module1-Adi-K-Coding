f2e3d0759b0e27b8e9ca46e1f31a3bb527b06c62
Adi-K-Coding <34950822+Adi-K-Coding@users.noreply.github.com>
2020-11-27 16:57:53 -0800

    logSearch

--- src/_06_Intro_To_Hash_Maps/_02_LogSearch.java
+++ src/_06_Intro_To_Hash_Maps/_02_LogSearch.java
@@ -33,11 +33,12 @@ public class _02_LogSearch implements ActionListener {
 	 */
 
 	HashMap<Integer, String> logSearch = new HashMap<Integer, String>();
-	JFrame frame = new JFrame();
+	JFrame frame = new JFrame("List o'Peeps");
 	JPanel panel = new JPanel();
-	JButton button1 = new JButton();
-	JButton button2 = new JButton();
-	JButton button3 = new JButton();
+	JButton button1 = new JButton("Add Entry");
+	JButton button2 = new JButton("Search by ID");
+	JButton button3 = new JButton("View List");
+	JButton button4 = new JButton("Remove Entry");
 
 	public static void main(String[] args) {
 		_02_LogSearch constructor = new _02_LogSearch();
@@ -49,11 +50,14 @@ public class _02_LogSearch implements ActionListener {
 		panel.add(button1);
 		panel.add(button2);
 		panel.add(button3);
+		panel.add(button4);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		button1.addActionListener(this);
 		button2.addActionListener(this);
 		button3.addActionListener(this);
+		button4.addActionListener(this);
+		frame.pack();
 	}
 
 	@Override
@@ -70,7 +74,24 @@ public class _02_LogSearch implements ActionListener {
 			String button2Input = logSearch.get(searchedIDNumber);
 			System.out.println(button2Input);
 		}
+		if (e.getSource() == (button3)) {
+			String allPeople = "";
+			for (Integer i : logSearch.keySet()) {
+				allPeople = allPeople + "ID: " + i + " Name: " + logSearch.get(i) + "\n";
+
+			}
+			JOptionPane.showMessageDialog(null, allPeople);
+		}
+		if (e.getSource() == (button4)) {
+			String userToRemoveString = JOptionPane.showInputDialog("Enter an ID Number to remove");
+			int userToRemoveInt = Integer.parseInt(userToRemoveString);
+			logSearch.remove(userToRemoveInt);
+		}
 	}
 
 }
 
+// When this is complete, add a fourth button to your window. Button 4: Remove
+// Entry When this button is clicked, prompt the user to enter an ID using an
+// input dialog. If this ID exists in the HashMap, remove it. Otherwise, notify
+// the user that the ID is not in the list.
